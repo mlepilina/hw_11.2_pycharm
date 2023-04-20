2aaa5d1666860417b061e61b1ab25ac3232cb5cb
maria <maria.vyukhina@mail.ru>
2023-04-20 15:53:28 +0400

    Adding new func in str_func.py, adding docstrings

--- str_func.py
+++ str_func.py
@@ -1,4 +1,10 @@
 
-def making_string_capitalized(word=input()):
+def making_string_capitalized(word):
+    '''Делает буквы слова заглавными'''
     return word.upper()
+
+
+def making_words_titled(line):
+    '''Делает заглавными первые буквы каждого слова в строке'''
+    return line.title()
 
